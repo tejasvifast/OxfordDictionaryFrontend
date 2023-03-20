c90cbe7323dcc516880207995a34b447b9426d1a
pragya <pragyatiwari831@gmail.com>
2023-03-21 00:37:30 +0530

    prod

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import './scss/style.scss'
 
-export const apiUrl= process.env.NODE_ENV==='development'? 'http://localhost:5000/':'https://warm-island-82496.herokuapp.com'
+export const apiUrl= process.env.NODE_ENV==='development'? 'http://localhost:5000/':'https://oxforddictionary-back.onrender.com/'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
